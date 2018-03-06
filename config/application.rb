68be2a72a520761988cc0e16c410fecdbfeef929
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-03-06 12:07:07 +0900

    [add] ungenerate test file when I will command rails g

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module MusicCalendar
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.test_framework false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
