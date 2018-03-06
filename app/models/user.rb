c3809d203ed3c32fa0b560dc7bab14aefc96f13f
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-03-06 18:19:05 +0900

    [add] modelにメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  devise :omniauthable
+
+  has_many :social_profiles, dependent: :destroy
+  def social_profile(provider)
+    social_profiles.select{ |sp| sp.provider == provider.to_s }.first
+  end
 end
